c8306655479f2cd355361a2fa3d0a91d527ccc31
chaoszhu(朱利军) <chaoszhu@lexin.com>
2019-08-14 14:29:41 +0800

    complete

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,26 @@
 <template>
-    <div>
-			<alert v-model="show" :title="'Tips'">
-				此demo用于调试微信JS-SDK功能<br><hr><br>
-				主要技术如下：<br><br>
-				1、前端：vue-cli2、vue、vue-router、axios、vux.ui等<br><br>
-				2、后端：基于node.js的express框架开发的获取微信签名的接口<br><br><hr>
-				因订阅号权限问题，获取wx签名采用测试账号提供的appId<br><br>
-				<!-- 在开发模式已开启wx.config的debug模式选项<br><br>
-				因此测试JS-SDK过程中可能会出现操作提示弹窗<br> -->
-			</alert>
-			<box gap="10px 10px">
-				<x-button :gradients="['#6F1BFE', '#9479DF']" @click.native="$router.push('/getUserInfo')">获取用户信息</x-button>
-				<x-button style="margin-top:20px;" :gradients="['#1D62F0', '#19D5FD']" @click.native="$router.push('/chooseImage')">微信图片功能</x-button>
-				<x-button :gradients="['#FF2719', '#FF61AD']" @click.native="$router.push('/record')">音频接口功能</x-button>
-				<x-button :gradients="['#c46156', '#56b9c4']" @click.native="$router.push('/getLocation')">获取用户位置</x-button>
-				<x-button :gradients="['#FF5E3A', '#FF9500']" @click.native="closeWindow">关闭网页</x-button>
-				<x-button :disabled='false' @click.native="scanQRCode">调用扫一扫(无权限)</x-button>
-				<x-button @click.native="closeWindow">关闭网页</x-button>
-				<x-button :disabled='true'>已加载自定义分享给朋友/朋友圈</x-button>
-			</box>
+    <div class="index">
+		<alert v-model="show" :title="'Tips'">
+			此demo用于调试微信JS-SDK功能<br><hr><br>
+			主要技术如下：<br><br>
+			1、前端：vue-cli2、vue、vue-router、axios、vux.ui等<br><br>
+			2、后端：基于node.js的express框架开发的获取微信签名的接口<br><br><hr>
+			因订阅号权限问题，获取wx签名采用测试账号提供的appId<br><br>
+			<span style="color:red;">
+				此微信网页Demo已开启wx.config的debug模式选项<br><br>
+				因此测试JS-SDK过程中可能会出现操作提示弹窗<br><br>
+				另外98%的功能测试正常不排除有小Bug<br>
+			</span>
+		</alert>
+		<box gap="10px 10px" class="box">
+			<x-button :gradients="['#6F1BFE', '#9479DF']" @click.native="$router.push('/getUserInfo')">获取用户信息</x-button>
+			<x-button style="margin-top:20px;" :gradients="['#1D62F0', '#19D5FD']" @click.native="$router.push('/chooseImage')">微信图片功能</x-button>
+			<x-button :gradients="['#FF2719', '#FF61AD']" @click.native="$router.push('/record')">音频接口功能</x-button>
+			<x-button :gradients="['#c46156', '#56b9c4']" @click.native="$router.push('/getLocation')">获取用户位置</x-button>
+			<x-button :gradients="['#FF5E3A', '#FF9500']" @click.native="closeWindow">关闭微信网页</x-button>
+			<x-button :disabled='false' @click.native="scanQRCode">调用扫一扫(无权限)</x-button>
+			<x-button :disabled='true'>已加载自定义分享给朋友/朋友圈</x-button>
+		</box>
     </div>
 </template>
 
@@ -33,21 +35,18 @@ export default {
 		},
 		data() {
 			return {
-				show: true,
-				shareStatus: '调试中~'
+				show: false
 			}
 		},
-		watch: {
-			// '$router': {
-			// 	handler: function (newVal) {
-			// 		console.log('new', newVal)
-			// 	},
-			// 	immediate: true
-			// }
-		},
 		mounted() {
+			if (!sessionStorage.getItem('show')) {
+				this.show = true
+				sessionStorage.setItem('show', true)
+			}
+
 			this.shareData()
-			// 如果存在code则保存到localStorage
+
+			// 如果存在code则保存到sessionStorage
 			try {
 				let href = location.href
 				let code = href.split('?')[1].split('&')[0].slice(5)
@@ -75,7 +74,7 @@ export default {
 			getUserInfo() {
 				let appId = localStorage.getItem('appId')
 				let url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURI('http://wx.zhulijun.club/')}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
-				window.location.href = url
+				location.href = url
 			},
 			// 分享（在加载完wx.config后调用一次即可）
 			shareData() {
@@ -111,5 +110,13 @@ export default {
 }
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+.index{
+	.box{
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+	}
+}
 </style>
